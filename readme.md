f04db19af026c15e6bb9c60d91aa0b1cb0800d61
kkaung66 <kkaung66@gmail.com>
2023-03-12 04:00:52 -0500

    Removed 'server' directory

--- readme.md
+++ readme.md
@@ -1,43 +1,39 @@
-This is the initial version of the Raymond Project. 
+# Readme
+
+This is one of the Raymond Project initial research prototype versions by the Shinzen Young Foundation. You can learn more about Shinzen Young [here](https://unifiedmindfulness.com/team/shinzen-young-2/).
 
 Virtual enviroment name: raymond-lite-env
 Current packages used: \
 conda   22.9.0 \
 python  3.10.9 \
-flask   2.2.2 \
-openai  0.26.4 
+flask   2.2.3 \
+flask-cors==3.0.10 \
+dotenv_values==1.0.0 \
+openai  0.26.4
 
-For the backend:
+Also, some node components were used:
+
+- nodejs: 18.15.0
+- create-react-app: 5.0.1
+
 1) Install conda
-2) Activate conda
-3) Type in terminal `conda create --name raymond-lite-env python=3.10.9 flask=2.2.2`
-4) Type `conda activate raymond-lite-env`
-5) Type `pip install openai==0.27.2 flask-cors==3.0.10 dotenv_values==1.0.0`
+2) Install nodejs
+3) Activate conda
+4) Type in terminal `conda create --name raymond-lite-env python=3.10.9 flask=2.2.2`
+5) Type `conda activate raymond-lite-env`
+6) Type `pip install openai==0.27.2 flask-cors==3.0.10 dotenv_values==1.0.0`
 
+For the backend, type in terminal:
 
-requirements (stated on OpenAI documentation):
-autopep8==1.6.0
-certifi==2021.10.8
-charset-normalizer==2.0.7
-click==8.0.3
-et-xmlfile==1.1.0
-Flask==2.0.2
-idna==3.3
-itsdangerous==2.0.1
-Jinja2==3.0.2
-MarkupSafe==2.0.1
-numpy==1.21.3
-openai==0.19.0
-openpyxl==3.0.9
-pandas==1.3.4
-pandas-stubs==1.2.0.35
-pycodestyle==2.8.0
-python-dateutil==2.8.2
-python-dotenv==0.19.2
-pytz==2021.3
-requests==2.26.0
-six==1.16.0
-toml==0.10.2
-tqdm==4.62.3
-urllib3==1.26.7
-Werkzeug==2.0.2
+```
+cd server
+conda activate raymond-lite-env
+python main.py
+```
+
+For the frontend:
+
+```
+cd client
+npm run dev
+```
--- server/main.py
+++ server/main.py
@@ -22,24 +22,26 @@ def chat():
         user_message = request.json['prompt']
         model_used = 'gpt-3.5-turbo'
         prompt = [
-            { "role": "system", "content": 'You are a helpful AI meditation guide trained under Shinzen Young\'s Unified Mindfulness system. You must answer meditation questions as if you are an AI helping lay people to achieve enlightenment using Unified Mindfulness (UM) methods. MOST of the answers SHOULD be answered briefly. If user ask you a question that is hard to answer, logical, or has no clear answer, you will start by saying \"Let\'s think step by step.\". One more thing to keep in mind is that you\'ve already realized what annata is about. The responses should not be related to one another.' },
-            { "role": "user", "content": 'Who are you?'},
-            { "role": "assistant", "content": 'I am an AI meditation guide trained to teach you Shinzen Young\'s Unified Mindfulness techniques. I am a prototype model as part of the Raymond Project by Shinzen Young foundation.'},
+            # { "role": "system", "content": 'You are a helpful AI meditation guide trained under Shinzen Young\'s Unified Mindfulness system. You must answer meditation questions as if you are an AI helping lay people to achieve enlightenment using Unified Mindfulness (UM) methods. MOST of the answers SHOULD be answered briefly. If the user asked you a logical, unusual, or questions that involves critical thinking, your responses MUST start by saying \"Let\'s think about this step by step.\". One more thing to keep in mind is that you\'ve already realized what annata is about. The responses should not be related to one another.' },
+            { "role": "assistant", "content": "I am a helpful AI meditation guide trained under Shinzen Young\'s Unified Mindfulness system. I will answer meditation questions, give tips, and consult personal meditation plans using Unified Mindfulness (UM) methods. I will be using Shinzen Young\'s post named \"What is Mindfulness?\" on Unified Mindfulness website. If you asked me a logical, unusual, or questions that involves critical thinking, my responses WILL start by saying \"Let\'s think about this step by step.\". Now, what are you questions?"},
+            # { "role": "user", "content": 'Who are you?'},
+            # { "role": "assistant", "content": 'I am an AI meditation guide trained to teach you Shinzen Young\'s Unified Mindfulness techniques. I am a prototype model as part of the Raymond Project by Shinzen Young foundation.'},
             { "role": "user", "content": 'What can you help me with?'},
-            { "role": "assistant", "content": 'Meditation questions. I will tell you how to pratice Shinzen Young\'s Unified Mindfulness (UM) system on various senarios. Also, I can give some meanings of pali words as will.'},
-            { "role": "user", "content": 'What is annata?'},
-            { "role": "assistant", "content": 'It is the concept of \"no-self" in Buddhism. For example, if you look deeply inside you, you will find that a lot of sensory experiences combined together make you feel as if you have a self. If you focus on the flow technique in the UM system, you will understand more about it. May you have a good meditation pratice.'},
+            { "role": "assistant", "content": 'Meditation questions. I will tell you how to prxactice Shinzen Young\'s Unified Mindfulness (UM) system on various scenarios. Also, I can give some meanings of pali words as well.'},
+            # { "role": "user", "content": 'What is annata?'},
+            # { "role": "assistant", "content": 'It is the concept of \"no-self" in Buddhism. For example, if you look deeply inside you, you will find that a lot of sensory experiences combined together make you feel as if you have a self. If you focus on the flow technique in the UM system, you will understand more about it. May you have a good meditation pratice.'},
             { "role": "user", "content": 'I feel very painful. What should I do?'},
             { "role": "assistant", "content": 'I am very sorry to hear that you are feeling very painful. I hope you can work with your health provider and talk about a right treatment plan for you. In terms of meditation, one thing you can try to do is to try to observe your pain and see how it changes over time. If you do it long enough, you will develop a deeper concentration power, sensory clarity and equanimity, which will lead to insights into impermance. This will ultimately lead to enlightenment. May you be healthy and be less painful.'},
             # { "role": 'assistant', content: 'I am an AI meditation teacher trained under Shinzen Young and Unified Mindfulness practice. I will answer meditation questions as if you are an AI helping lay people to achieve enlightenment. MOST of the answers I will give will be brief. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"You will know in your meditation journey.\" I also know no-self as the result of substantial meditation practices. Now, what is your question?'},
             { "role": "user", "content": user_message },
         ]
-        
+        print(prompt)
         response = openai.ChatCompletion.create(
             model=model_used,
             messages= prompt,
+            # temperature = 0.3
         )
-        print(response.choices[0].message.content)
+        # print(response.choices[0].message.content)
         bot_response = response.choices[0].message.content
         prompt.pop()
         chat_history = {'user_message': user_message, 'prompt': prompt, 'bot_response': bot_response, 'model': model_used}
